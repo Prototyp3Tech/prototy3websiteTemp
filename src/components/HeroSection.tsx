31251ea304f8c9cc2302a65df7b5004429db0845
khush938 <67571400+khush938@users.noreply.github.com>
2025-09-11 15:55:34 -0400

    Hero fixes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,18 +14,18 @@ const HeroSection: React.FC<HeroSectionProps> = ({
 
   return (
     <section className="text-gray-800 sm:pt-20 pt-0 min-h-screen relative overflow-hidden" id="hero">
-      <div className="mx-auto px-3 px-5 pt-5 max-w-[80vw] md:ml-[15vw]">
-        <div className="grid grid-cols-1 lg:grid-cols-2 min-h-[80vh] gap-[0px]">
+      <div className="mx-auto px-3 px-5 pt-5 max-w-[80vw] md:ml-[20vw]">
+        <div className="grid grid-cols-1 lg:grid-cols-2 min-h-[80vh] gap-[0px] screenFix">
           {/* Content Section */}
           <div className="flex justify-center align-top w-auto max-w-none max-h-[60vh] breakpointFix lg:max-h-[55vh] lg:justify-start order-1 lg:order-1 z-10">
             <div className="transWhite p-4 sm:p-6 md:p-8 lg:p-12 rounded-xl sm:rounded-2xl shadow-2xl">
               <div className="flex items-start">
-                <span className="text-orange-500 font-extrabold text-4xl lg:text-6xl mr-4">{'{'}</span>
+                <span className="text-orange-500 font-extrabold text-xl lg:text-5xl mr-4">{'{'}</span>
                 <div className="flex-1">
-                  <h1 className="breakpointFix text-4xl lg:text-6xl font-bold leading-tight text-gray-700" style={{color: '#1F2937'}}>
+                  <h1 className="textFix text-6xl font-bold leading-tight text-gray-700" style={{color: '#1F2937'}}>
                    Zero Experience? 
                   </h1> 
-                  <h1 className="breakpointFix text-4xl lg:text-6xl font-bold mb-4 sm:mb-6 leading-tight text-gray-700 -mt-2" style={{color: '#1F2937'}}>
+                  <h1 className="textFix text-6xl font-bold mb-4 sm:mb-6 leading-tight text-gray-700 -mt-2" style={{color: '#1F2937'}}>
                    Perfect. Let's Build.
                   </h1>
                   <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-6 sm:mb-8 text-gray-600 leading-relaxed" style={{color: '#4C545F'}}>
@@ -52,9 +52,9 @@ const HeroSection: React.FC<HeroSectionProps> = ({
           </div>
           
           {/* Illustration Section */}
-          <div className="flex justify-center self-end lg:justify-start h-full md:ml-[-3vw] md:max-h-[55vh] order-2 lg:order-2">
+          <div className="flex justify-center self-end lg:justify-start picContFix h-full md:ml-[-3vw] order-2 lg:order-2 z-10">
             <div className="w-full max-w-md lg:max-w-lg xl:max-w-xl">
-              { <img src={heroRightSvg2} alt={illustrationAlt} className="h-full w-[25vw] rounded-xl sm:rounded-2xl heroRightSvg"/>}
+              { <img src={heroRightSvg2} alt={illustrationAlt} className="h-full w-[25vw] picFix rounded-xl sm:rounded-2xl"/>}
             </div>
           </div>
         </div>
